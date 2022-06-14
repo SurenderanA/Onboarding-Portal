58a6e0275ff120fcbdd07994f5d9ac24ef55e3d1
SAdikes1 <Surenderan.Adikesavan1@T-Mobile.com>
2022-06-14 15:24:23 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { ToastrService } from 'ngx-toastr';
+
+
+const URL = 'http://localhost:8080/api/upload';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-node-file-upload';
+
+  public uploader: FileUploader = new FileUploader({
+    url: URL,
+    itemAlias: 'image'
+  });
+
+  constructor(private toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+    this.uploader.onCompleteItem = (item: any, status: any) => {
+      console.log('Uploaded File Details:', item);
+      console.log('Uploaded File Details:', status);
+      this.toastr.success('File successfully uploaded!');
+    };
+  }
+
 }
